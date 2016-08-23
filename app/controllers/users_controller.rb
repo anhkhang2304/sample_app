24bdb8faa6b668a014316f34578c5dc88ea6e916
anhkhang2304 <ngo.anh.khang@framgia.com>
2016-08-23 13:27:37 +0700

    Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate page: params[:page], per_page: 10
+    @users = User.where(activated: true).paginate page: params[:page], per_page: 10
   end
 
   def new
@@ -14,13 +14,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by id: params[:id]
+    redirect_to root_url and return unless FILL_IN
   end
 
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = "Register successful!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render :new
     end
@@ -34,16 +36,6 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
-  end
-
-  def create
-    @user = User.new user_params
-    if @user.save
-      flash[:success] = "Register successful!"
-      redirect_to @user
-    else
-      render :new
-    end
   end
 
   def edit
